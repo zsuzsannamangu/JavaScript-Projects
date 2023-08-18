26e44fe7c589267d978582e57cdbb4e0adb6a88a
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2023-08-17 18:38:09 -0700

    Created new folder
    New folder with index.html and main.js
    Added functions to practice math operations

---
+++ Basic-Javascript-Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,9 @@
+function Addition() {
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML="2 + 2 = " + addition;
+}
+
+function Multiplication() {
+    var multiply = 2 * 2;
+    document.getElementById("Next").innerHTML="2 * 2 = " + multiply;
+}
