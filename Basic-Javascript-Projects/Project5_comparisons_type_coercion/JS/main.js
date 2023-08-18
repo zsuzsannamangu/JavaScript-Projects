328cdc4df4f8874e5a715488fb14927fe586a7a4
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2023-08-18 10:21:44 -0700

    updated index.html and main.js
    Added isNaN function

--- Basic-Javascript-Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic-Javascript-Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,3 +1,12 @@
 document.write(typeof "Word"); //typeof operator displays data type, here output is string
+document.write("<br>"); //new line
 document.write(typeof 5.5);
-document.write("10" + 2);
+document.write("<br>");
+document.write("10" + 2); //type coercion
+document.write("<br>");
+document.write(0/0); //returns NaN because zero can't be divided by zero
+document.write("<br>");
+document.write(isNaN("Hello")); //returns true, because Hello is indeed not a number
+document.write("<br>");
+document.write(isNaN(5)); //returns false, because 5 is a number
+document.write("<br>");
