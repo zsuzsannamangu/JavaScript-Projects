71d55f8a969928e3ebe332a8ba8a7009756dd635
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2023-08-17 17:32:23 -0700

    Added comments

--- Basic-Javascript-Projects/Project2_functions/JS/main.js
+++ Basic-Javascript-Projects/Project2_functions/JS/main.js
@@ -1,10 +1,12 @@
-function displayMessage() {
-    var str = "This is your message.";
-    document.getElementById("message").innerHTML = str;
+function displayMessage() { //created the displayMessage function
+    var str = "This is your message."; //created a variable, added the message here that will be displayed to the user
+    document.getElementById("message").innerHTML = str; //we want to display the HTML element that has the message Id. 
+                                                        //That element should have the content of the variable named str
 }
 
 function myFunction() {
-    var sentence = "This is my sentence";
-    sentence += " that I want to concatenate.";
-    document.getElementById("together").innerHTML = sentence;
+    var sentence = "This is my sentence"; //created variable
+    sentence += " that I want to concatenate."; //added more lines to that variable
+    document.getElementById("together").innerHTML = sentence; //innerHTML modifies the content of an HTML element, in this case the element that has the together id. 
+                                                                //That will be modified to whatever the variable called sentence holds.
 }
