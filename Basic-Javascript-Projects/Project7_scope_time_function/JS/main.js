4b12897eff69eec8966417d7c007316226e49649
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2023-08-18 13:38:59 -0700

    New folder
    Added functions to practice global and local variables

---
+++ Basic-Javascript-Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,19 @@
+var c = 5;
+var d = 4;
+
+function Subtraction () {
+    var subtract = c - d;
+    document.getElementById("subtract_Numbers").innerHTML = "5 - 4 is " + subtract;
+}
+
+function Addition() {
+    var a = 2;
+    var b = 2;
+    var add = a + b + c;
+    document.getElementById("add_Numbers").innerHTML = "2 + 2 + 5 is " + add;
+}
+
+function Multiplication() {
+    var multiply = a * c;
+    document.getElementById("multiply_Numbers").innerHTML = "2 * 5 is " + multiply;
+}
