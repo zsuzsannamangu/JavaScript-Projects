0f21194201d0929a206959b1fd24728867c57b60
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2023-08-18 09:52:21 -0700

    Update main.js

--- Basic-Javascript-Projects/Project4_dictionaries/JS/main.js
+++ Basic-Javascript-Projects/Project4_dictionaries/JS/main.js
@@ -1,10 +1,11 @@
-function plant_Dictionary() {
-    var Plant = {
+function plant_Dictionary() { //created function named plant_Dictionary()
+    var Plant = { //created dictionary
         Name: "Philodendron",
         Type: "Congo",
         Pot_color: "Red",
         Place: "Room3",
     };
-    delete Plant.Place;
-    document.getElementById("dictionary").innerHTML = Plant.Place;
+    delete Plant.Place; //deleted a KVP from the dictionary
+    document.getElementById("dictionary").innerHTML = Plant.Place; //asked to display one KVP of the dictionary
+    //since it's now deleted Plant.Place will be displayed as undefined
 }
