7760df3bdd516a2e4cfac92a7c33134a3c4bed42
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2023-08-17 17:25:29 -0700

    Added a new function
    to practice concatenate

--- Basic-Javascript-Projects/Project2_functions/JS/main.js
+++ Basic-Javascript-Projects/Project2_functions/JS/main.js
@@ -2,3 +2,9 @@ function displayMessage() {
     var str = "This is your message.";
     document.getElementById("message").innerHTML = str;
 }
+
+function myFunction() {
+    var sentence = "This is my sentence";
+    sentence += " that I want to concatenate.";
+    document.getElementById("together").innerHTML = sentence;
+}
