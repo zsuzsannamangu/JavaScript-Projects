47251dc9e21791fe1957457245606db895ffee3b
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2023-08-18 11:01:51 -0700

    Update main.js

--- Basic-Javascript-Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic-Javascript-Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -17,10 +17,14 @@ document.write("<br>");
 document.write(isNaN(5)); //returns false, because 5 is a number
 document.write("<br>");
 document.write("<br>");
+
 document.write(2E310); //displays Infinity
 document.write("<br>");
 document.write(-3E310); //displays negative Infinity
 document.write("<br>");
+document.write("<br>");
+
+document.write("Boolean logic:");
 document.write("<br>");
 document.write(10 > 2); //Boolean logic - true since 10 is greater than 2
 document.write("<br>");
@@ -40,24 +44,29 @@ Y="12";
 Z=82;
 A="82";
 B=22;
-document.write(X===Z);
+document.write(X===Z); //data type and value are the same, so it's true
 document.write("<br>");
-document.write(X === Y);
+document.write(X === Y); //data type and value are both different, so it's false
 document.write("<br>");
-document.write(X===A);
+document.write(X===A); //data type is different, so it's false
 document.write("<br>");
-document.write(Z===B);
+document.write(Z===B); //data type is the same, but the value is different, so it's false
+document.write("<br>");
+document.write("<br>");
 
-
-
-
-
-
+document.write("Three Boolean logical operators: AND &&, OR ||, NOT !:");
 document.write("<br>");
+document.write(10 > 5 && 5 > 3); //when using the AND operator, both need to be true for it to be true
 document.write("<br>");
+document.write(10 > 5 && 5 > 6);
 document.write("<br>");
+document.write(!(10>5)); //This is the NOT operator
 document.write("<br>");
+document.write(!(5>10));
 document.write("<br>");
+document.write(10>5 || 5>10); //This is the OR operator, one needs to be true for it to be true
+document.write("<br>");
+document.write(2>4 || 1>5);
 
 function displayInfinity() {
     var a = 2E322;
@@ -66,5 +75,5 @@ function displayInfinity() {
     document.getElementById("infinity2").innerHTML = b; //displays negative Infinityxs
 }
 
-console.log(2+2);
+console.log(2+2); //displays in the console
 console.log(2>5);
