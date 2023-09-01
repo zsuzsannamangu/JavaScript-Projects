a1838e1939b5890d031bdb1b20ab9e62615e1c4f
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2023-09-01 13:08:26 -0700

    Added data*- attribute

--- Basic-Javascript-Projects/Project12/JS/main.js
+++ Basic-Javascript-Projects/Project12/JS/main.js
@@ -1,7 +1,13 @@
 function validateForm() {
     let x = document.forms["myForm"] ["firstname"].value;
-    if (x == "") { //If a form field (firstname) is empty
+    if (x == "") { //If a form field (firstname here) is empty
         alert("Name must be filled out"); //this function alerts a message
         return false; //and returns false, to prevent the form from being submitted
     }
+}
+
+function showType(animal) { //created function with object "animal"
+    var animalType = animal.getAttribute("data-animal-type"); //created variable that equals to the data attribute
+    //The getAttribute() method returns the value of an element's attribute.
+    alert("A(n) " + animal.innerHTML + " is a(n) " + animalType + ".");
 }
