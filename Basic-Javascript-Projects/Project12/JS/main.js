2b300065ac55c4faf6cb6f8aefea12bcc83252f5
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2023-09-01 15:14:14 -0700

    Updated main.js

--- Basic-Javascript-Projects/Project12/JS/main.js
+++ Basic-Javascript-Projects/Project12/JS/main.js
@@ -6,8 +6,25 @@ function validateForm() {
     }
 }
 
-function showType(animal) { //created function with object "animal"
+showType = function(animal) {
     var animalType = animal.getAttribute("data-animal-type"); //created variable that equals to the data attribute
     //The getAttribute() method returns the value of an element's attribute.
     alert("A(n) " + animal.innerHTML + " is a(n) " + animalType + ".");
 }
+
+//arrow function:
+//showType = (animal) => {
+    //var animalType = animal.getAttribute("data-animal-type"); //created variable that equals to the data attribute
+    //The getAttribute() method returns the value of an element's attribute.
+   //alert("A(n) " + animal.innerHTML + " is a(n) " + animalType + ".");
+//}
+
+
+var ages = [2, 10, 18, 20, 13, 15, 30];
+//using the arrow function to pass in parameter of age
+checkAge = (age) => age >= 18;
+function myFunction() {
+    //Use the array .some() method to iterate thru the array of ages and display the results.
+    document.getElementById('displayAge').innerHTML = ages.some(checkAge);
+}
+
